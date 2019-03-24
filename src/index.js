524cb4158cb625e34e005e7114e6fae52b67fc4e
Vitaliy Prohorenko <GoonzaPVS@yandex.by>
2019-03-24 23:45:04 +0300

    feat: add function getLoveTrianglesCount which calculates how many love triangles phenomenons take place

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var loveTriangles=0;
+  var firstSpichonee;
+  var secondSpichonee;
+  var thirdSpichonee;
+
+  for (var i = 0; i < preferences.length; i++) {
+    firstSpichonee = preferences[i];
+    secondSpichonee = preferences[firstSpichonee-1];
+    thirdSpichonee = preferences[secondSpichonee-1];
+    if ((thirdSpichonee-1 == i) && (firstSpichonee!=secondSpichonee) && (secondSpichonee!=thirdSpichonee))
+    loveTriangles++;
+  }
+
+  return loveTriangles/3;
 };
